507710d4f64ecfea515f77a3829b15feb3d28848
mrmodise <modisemorebodi@gmail.com>
2017-08-19 17:05:48 +0200

    added easier implementation of an Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
-alert("Welcome")
+import {Observable} from 'rxjs';
+import {Observer} from 'rxjs/Observer';
+
+let numbers = [1,5,10];
+
+// create observable from the array of numbers
+let source = Observable.from(numbers);
+
+// an easier implementation
+source.subscribe(
+    value => console.log(`Value ${value}: easier implementation`),
+    error => console.log(`On error ${error}: easier implementation`),
+    () => console.log('Completed: easier implementation')
+);
+
+// formal implementation
+class MyObserver implements Observer<number>{
+
+    next(value){
+        console.log(`Value ${value}`);
+    }
+
+    error(e){
+        console.log(`On error ${e}`);
+    }
+
+    complete(){
+        console.log('Completed')
+    }
+}
+
+// subscript to the observable
+source.subscribe(new MyObserver());
